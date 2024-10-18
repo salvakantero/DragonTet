7908b8d0f1118323a853fd0155aadb48fa276340
salvaKantero <91732978+salvakantero@users.noreply.github.com>
2024-10-18 22:24:16 +0200

    -

--- t4d.c
+++ t4d.c
@@ -4,8 +4,13 @@
 
 #define SCREEN_WIDTH 32 // ancho de la pantalla en modo texto
 #define SCREEN_BASE_ADDR 0x0400 // dirección base de la memoria de video en modo texto
-#define KEYBOARD_STATUS 0xFF02 // dirección de registro de estado del teclado
-#define KEYBOARD_DATA 0xFF00 // dirección de registro de datos del teclado
+
+#define LASTSIDEBLOCK 3 // para recorrer con bucles la pieza actual (0 a 3)
+#define SIDEBLOCKCOUNT 4 // tamano del lado de la pieza (4x4)
+#define NOBLOCK '0' // carácter que representa un bloque vacío
+#define PITTOP 0 // pos Y de la parte de arriba del foso (justo arriba de la pantalla)
+#define PITWIDTH 10 // ancho del foso
+#define PITHEIGHT 16 // alto del foso
 
 /*
 unsigned char:	byte
@@ -20,44 +25,44 @@ byte key;
 byte i;
 byte numPlayers;
 byte pitLeft[2];
-char names[7][11] = {"DRAGON1","DRAGON2","DRAGON3","DRAGON4","DRAGON5","",""};
-word scores[7] = {1400, 1300, 1200, 1100, 100, 0, 0};
+char names[7][11] = {"DRAGON","DRAGON","DRAGON","DRAGON","DRAGON","",""};
+word scores[7] = {1400, 1300, 1200, 1100, 1000, 0, 0};
+char pit[2][PITWIDTH * PITHEIGHT];
 
+/*
 void drawString(word x, word y, const char *str) {
     char *screenPos = (char *) (SCREEN_BASE_ADDR + x + y * SCREEN_WIDTH);
     memcpy(screenPos, str, strlen(str));
-}
+}*/
 
 void drawHighScores() {
-	char strScore[20];
     for(i = 0; i < 5; i++) {
-        drawString(8, 8 + i, "................");
-        drawString(8, 8 + i, names[i]);
-		sprintf(strScore, "%5d", scores[i]);
-        drawString(20, 8 + i, strScore);
+        locate(7, 8 + i); printf("................");
+        locate(7, 8 + i); printf("%s", names[i]);
+        locate(19, 8 + i); printf("%5d", scores[i]);
 	}
-    drawString(2, 14, "PRESS ANY KEY TO CONTINUE...");
+	locate(2,14); printf("PRESS ANY KEY TO CONTINUE...");
 	waitkey(0);
 }
 
 void drawHeader() {
-	cls(1);
-    drawString(9, 1, "***************");
-    drawString(9, 2, "* T E T R I S *");
-    drawString(9, 3, "***************");
-    drawString(8, 5, "SALVAKANTERO 2024");
+    locate(8, 1); printf("***************");
+    locate(8, 2); printf("* T E T R I S *");
+    locate(8, 3); printf("***************");
+    locate(7, 5); printf("SALVAKANTERO 2024");
 }
 
 void drawMenu() {
+	cls(1);
 	drawHeader();
-	drawString(8, 8, "1)  1 PLAYER GAME");
-    drawString(8, 9, "2)  2 PLAYER GAME");
-    drawString(8, 10, "3)  HIGH SCORES");
-    drawString(8, 11, "4)  EXIT");
-    drawString(7, 14, "SELECT OPTION (1-4)");	
+	locate(7, 8);  printf("1)  1 PLAYER GAME");
+    locate(7, 9);  printf("2)  2 PLAYER GAME");
+    locate(7, 10); printf("3)  HIGH SCORES");
+    locate(7, 11); printf("4)  EXIT");
+    locate(6, 14); printf("SELECT OPTION (1-4)");	
 }
 
-void Menu() {
+void menu() {
 	drawMenu();
     do {
         key = inkey();		
@@ -80,23 +85,33 @@ void Menu() {
     } while (TRUE);
 }
 
-void Init() {
-    // lógica para inicializar el sistema y los fosos
-    Menu();
+// lógica para inicializar el sistema y los fosos
+void init() {
+    menu();
     pitLeft[0] = 1;
     pitLeft[1] = 23;
+	for(i = 0; i <= numPlayers; i++) {
+        gameOver(i) = FALSE; // partida en curso
+        level(i) = 1; // nivel inicial
+        lines(i) = 0 // lineas conseguidas
+        scores(i + 5) = 0; // puntuacion actual
+        nextShape(i) = -1; // sera necesario generar pieza
+        memset(pit(i), NOBLOCK, PITWIDTH * PITHEIGHT); // inicializa el foso vacío
+        createShape(i); // genera pieza (forma, posición)
+        drawPit(i); //pinta foso
+    }
+    displayStatus();
 }
 
-void CheckScores(byte player) {
-    // Lógica para verificar si las puntuaciones son de TOP 5
+// lógica para verificar si las puntuaciones son de TOP 5
+void checkScores(byte player) {
 }
 
 int main(void) {
     //while (1) {
-        Init(); // Inicializa el sistema y arranca el bucle principal
-        // Después del bucle principal, verifica si las puntuaciones están en el TOP 5
+        Init();
         for (i = 0; i <= numPlayers; i++)
-            CheckScores(i);
+            checkScores(i);
     //}
     return 0;
 }
