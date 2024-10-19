cbbf3cc54728e0573699cddb61cfc97902f2605e
salvaKantero <91732978+salvakantero@users.noreply.github.com>
2024-10-19 23:36:36 +0200

    -

--- t4d.c
+++ t4d.c
@@ -24,10 +24,14 @@ signed long:	sdword
 byte key;
 byte i;
 byte numPlayers;
+byte gameOver[2];
+byte level[2];
+byte lines[2];
 byte pitLeft[2];
+char pit[2][PITWIDTH * PITHEIGHT];
+sbyte nextShape[2];
 char names[7][11] = {"DRAGON","DRAGON","DRAGON","DRAGON","DRAGON","",""};
 word scores[7] = {1400, 1300, 1200, 1100, 1000, 0, 0};
-char pit[2][PITWIDTH * PITHEIGHT];
 
 /*
 void drawString(word x, word y, const char *str) {
@@ -35,10 +39,68 @@ void drawString(word x, word y, const char *str) {
     memcpy(screenPos, str, strlen(str));
 }*/
 
+void displayStatus() {
+	/*
+    Color 0, 2
+
+    'player 1
+    If GameOver(0) = TRUE Then
+        Locate 8, PITLEFT(0)
+        Print "GAME OVER!"
+    End If
+    Locate 2, 12: Print "=PLAYER 1="
+    Locate 3, 12: Print "Level:" + Str$(Level(0)) 'pinta el num. de nivel
+    Locate 4, 12: Print "Lines:" + Str$(Lines(0)) 'pinta las lineas
+    Locate 5, 12: Print "Sc:" + Str$(Scores(5)) 'pinta la puntuacion
+    Locate 6, 12: Print "Next:"
+
+    'player 2
+    If NumPlayers = 1 Then
+        If GameOver(1) = TRUE Then
+            Locate 8, PITLEFT(1)
+            Print "GAME OVER!"
+        End If
+        Locate 10, 12: Print "=PLAYER 2="
+        Locate 11, 12: Print "Level:" + Str$(Level(1)) 'pinta el num. de nivel
+        Locate 12, 12: Print "Lines:" + Str$(Lines(1)) 'pinta las lineas
+        Locate 13, 12: Print "Sc:" + Str$(Scores(6)) 'pinta la puntuacion
+        Locate 14, 12: Print "Next:"
+    End If
+
+    For i = 0 To NumPlayers
+        If GameOver(i) = FALSE Then DrawNextShape i
+    Next i */
+}
+
+void drawPit(i) {
+	/*
+    'repinta el contenido del foso
+    For PitY = 0 To PITHEIGHT - 1
+        For PitX = 0 To PITWIDTH - 1
+            BlockColor$ = Mid$(Pit$(i), ((PITWIDTH * PitY) + PitX) + 1, 1)
+            DrawBlock BlockColor$, PitX, PitY, i
+        Next PitX
+    Next PitY */
+}
+
+void createShape(i) {
+	/*
+    DropRate!(i) = 1 - (Level(i) * 0.2) 'tiempo de caida variable segun el nivel
+    If DropRate!(i) <= 0 Then DropRate!(i) = .1 'limite de caida de .1
+    'si no es la primera pieza la toma de NextShape
+    'si es la primera la genera (0 a 6)
+    If NextShape(i) >= 0 Then Shape(i) = NextShape(i) Else Shape(i) = Int(Rnd * 7)
+    ShapeAngle(i) = 0
+    ShapeMap$(i) = GetRotatedShapeMap$(Shape(i), ShapeAngle(i)) 'composicion de la pieza
+    ShapeX(i) = 3 'posicion X inicial (centro del foso)
+    ShapeY(i) = -SIDEBLOCKCOUNT 'posicion Y inicial (totalmente oculta)
+    CreateNextShape (i) */
+}
+
 void drawHighScores() {
     for(i = 0; i < 5; i++) {
-        locate(7, 8 + i); printf("................");
-        locate(7, 8 + i); printf("%s", names[i]);
+        locate(7, 8 + i);  printf("................");
+        locate(7, 8 + i);  printf("%s", names[i]);
         locate(19, 8 + i); printf("%5d", scores[i]);
 	}
 	locate(2,14); printf("PRESS ANY KEY TO CONTINUE...");
@@ -91,12 +153,12 @@ void init() {
     pitLeft[0] = 1;
     pitLeft[1] = 23;
 	for(i = 0; i <= numPlayers; i++) {
-        gameOver(i) = FALSE; // partida en curso
-        level(i) = 1; // nivel inicial
-        lines(i) = 0 // lineas conseguidas
-        scores(i + 5) = 0; // puntuacion actual
-        nextShape(i) = -1; // sera necesario generar pieza
-        memset(pit(i), NOBLOCK, PITWIDTH * PITHEIGHT); // inicializa el foso vacío
+        gameOver[i] = FALSE; // partida en curso
+        level[i] = 1; // nivel inicial
+        lines[i] = 0; // lineas conseguidas
+        scores[i+5] = 0; // puntuacion actual
+        nextShape[i] = -1; // sera necesario generar pieza
+        memset(pit[i], NOBLOCK, PITWIDTH * PITHEIGHT); // inicializa el foso vacío
         createShape(i); // genera pieza (forma, posición)
         drawPit(i); //pinta foso
     }
@@ -109,7 +171,7 @@ void checkScores(byte player) {
 
 int main(void) {
     //while (1) {
-        Init();
+        init();
         for (i = 0; i <= numPlayers; i++)
             checkScores(i);
     //}
